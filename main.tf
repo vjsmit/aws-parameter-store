b07313117014d186b8bd659cc51632dc625683c1
vjsmit <vj.smit97@gmail.com>
2023-06-18 13:00:05 +0530

    aws-ssm

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "demo1", value = "demo1", type = "string" },
-    { name = "demo2", value = "demo2", type = "string" }
+    { name = "demo1", value = "demo1", type = "String" },
+    { name = "demo2", value = "demo2", type = "String" }
   ]
 }
