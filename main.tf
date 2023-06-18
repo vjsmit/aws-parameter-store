571b154a81ae6ddbdfdb2a5520dfcc0f2921d5ca
vjsmit <vj.smit97@gmail.com>
2023-06-18 14:50:32 +0530

    aws-ssm

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.smitdevops.online", type = "String" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.smitdevops.online", type = "String" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.smitdevops.online", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.smitdevops.online", type = "String" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.smitdevops.online", type = "String" }
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.smitdevops.online:8080", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.smitdevops.online:8080", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.smitdevops.online:8080", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.smitdevops.online:8080", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.smitdevops.online:8080", type = "String" }
   ]
 }
