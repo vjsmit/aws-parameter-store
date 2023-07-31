2a8700e5d860c1633dc06baeb8e97c8ff0f0d02c
vjsmit <vj.smit97@gmail.com>
2023-07-31 23:12:12 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -31,10 +31,12 @@ variable "params" {
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.smitdevops.online", type = "String" },
     { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.smitdevops.online:27017/users", type = "String" },
     { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
 
 
     { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
     { name = "roboshop.dev.dispatch.amqp_pass", value = "roboshop123", type = "SecureString" },
     { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
   ]
 }
